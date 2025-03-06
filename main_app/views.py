320082c2474ae632c25e091e04b7a2f22455d740
Tanishq Harchandani <tharchandani93@gmail.com>
2025-03-06 22:34:34 +0530

    email update
    email update working

--- main_app/views.py
+++ main_app/views.py
@@ -6,5 +6,17 @@ from django.conf import settings
 # Create your views here.
 def index_page(request):
     
+    if request.method == 'POST':
+       first_name = request.POST.get('first_name')
+       
+       message = request.POST.get('message')
+       last_name = request.POST.get('last_name')
+       emaill = request.POST.get('emaill')
+       
+       send_mail('Contact Form',
+        f"first name:- {first_name}\n last name:- {last_name}\n message:- {message}\n email:- {emaill}",
+        settings.EMAIL_HOST_USER,
+        ['kunaladwani1456@gmail.com'], 
+        fail_silently=False)
 
     return render(request,"index.html")
